af214f7591f9eac20802f02ad3e8065f0ad80e16
Connie Bernardin <conniebernardin@googlemail.com>
2022-01-28 09:28:48 +0000

    Arrays Exercises basic

---
+++ src/com/bnta/week_two_thur/ArraysExercises.java
@@ -0,0 +1,22 @@
+package com.bnta.week_two_thur;
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        // declare an empty integer array of length = 3
+        int[] emptyArray = new int[3];
+        System.out.println(Arrays.toString(emptyArray));
+        // using your array from Q1, fill it with the number `4`
+        // print the array to the console
+        emptyArray [0] = 4;
+        emptyArray [1] = 4;
+        emptyArray [2] = 4;
+        System.out.println(Arrays.toString(emptyArray)); //convert to string to print array
+
+
+        // declare and initialise a String array which holds the values "a", "b", "c" and "d"
+        // print the array to the console
+
+    }
+}
